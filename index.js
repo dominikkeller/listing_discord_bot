de4b0d4d8ebe129812c91774420a9c05231429ea
Dominik Keller <39487564+dominikkeller@users.noreply.github.com>
2022-04-09 15:35:14 +0200

    feat: Functionality

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const { Intents } = require("discord.js");
+
 const OpenseaScraper = require("opensea-scraper");
 require("dotenv").config(); //initialize dotenv
 
@@ -10,41 +12,69 @@ var localStorage = new LocalStorage("./offers");
 
 const SLUG = process.env.SLUG;
 const CLIENT_TOKEN = process.env.CLIENT_TOKEN;
+const USERNAME = process.env.USERNAME;
 
 // options
 const options = {
-  debug: true,
-  logs: true,
+  debug: false,
+  logs: false,
   sort: true,
   browserInstance: undefined,
 };
 
-const client = new Discord.Client({ intents: [] });
+const client = new Discord.Client({
+  intents: [Intents.FLAGS.GUILD_MESSAGES],
+});
 client.login(CLIENT_TOKEN);
 
 client.on("ready", async () => {
-  const basicInfo = await OpenseaScraper.basicInfo(SLUG, options);
-  //client.user.setAvatar(basicInfo.imageUrl);
+  let basicInfo = await OpenseaScraper.basicInfo(SLUG, options);
+  client.user.setAvatar(basicInfo.imageUrl);
+  client.user.setUsername(USERNAME);
 
-  let result = await OpenseaScraper.offers(SLUG, options);
-  await saveOffers(result.offers);
-  await getOffers();
-  /*setInterval(async function () {
+  setInterval(async function () {
     try {
-        let result = await OpenseaScraper.offers(slug, options);
-        console.log(result);
-      
+      let result = await OpenseaScraper.offers(SLUG, options);
+      const upToDateOffers = result.offers;
+      await saveOffers(upToDateOffers);
+      await checkNewOffers(upToDateOffers);
     } catch (e) {
       console.log(e.toString());
     }
-  }, 4000);*/
+  }, 120000);
+});
+
+client.on("messageCreate", async (msg) => {
+  switch (msg.content) {
+    case "!offers":
+      const offers = await getOffers();
+      offers.map((offer) =>
+        msg.channel.send(
+          `${offer.name} for ${offer.floorPrice.amount} ETH: => ${offer.offerUrl} \r\n`
+        )
+      );
+      break;
+  }
 });
 
 async function getOffers() {
   const offers = localStorage.getItem("offers");
-  console.log(JSON.parse(offers));
+  return JSON.parse(offers);
 }
 
 async function saveOffers(data) {
   localStorage.setItem("offers", JSON.stringify(data)); //store colors
+}
+
+async function checkNewOffers(data) {
+  const savedOffers = await getOffers();
+
+  savedOffers.forEach(async (offer) => {
+    if (!(data.filter((t) => t.tokenId === offer.tokenId).length > 0)) {
+      const channel = await client.channels.fetch("961995338670047325");
+      channel.send(
+        `${offer.name} for ${offer.floorPrice.amount} ETH: => ${offer.offerUrl} \r\n`
+      );
+    }
+  });
 }
